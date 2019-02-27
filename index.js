0688b03f4dd70c6f573c9dc8fb08805e765edfd6
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-27 23:19:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
-let scruberGreetingForFeet
-
-if scuberGreetingForFeet( < 400) {
-  scruberGreetingForFeet = 'This one is on me!'
+function scuberGreetingForFeet (feet) {
+  if (feet < 400) {
+    return 'This one is one me!'
+  }
 }
