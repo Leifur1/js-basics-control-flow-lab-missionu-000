1b34f57b05f10ceb0d4f1e4ac8c88d39fe6c1819
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-27 23:18:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 let scruberGreetingForFeet
 
-if (scruberGreetingForFeet < 400) {
+if scuberGreetingForFeet( < 400) {
   scruberGreetingForFeet = 'This one is on me!'
 }
